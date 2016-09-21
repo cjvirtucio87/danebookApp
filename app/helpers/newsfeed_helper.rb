49c69a7d31d8c4b8b82da50582b98ee0e6e9f796
cjvirtucio87 <cjv287@gmail.com>
2016-09-20 22:24:45 -0700

    Newsfeed ready.

--- app/helpers/newsfeed_helper.rb
+++ app/helpers/newsfeed_helper.rb
@@ -1,2 +1,12 @@
 module NewsfeedHelper
+
+  def activity_message(activity)
+    case activity.trackable
+    when Post
+      return link_to "made a new post!", timeline_path(activity.trackable.user.timeline)
+    when Comment
+      return link_to "made a new comment!", timeline_path(activity.trackable.user.timeline)
+    end
+  end
+
 end
